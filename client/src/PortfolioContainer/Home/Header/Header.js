43ac678b78e898f93fb24a50fb1b0d7eb2bffec9
swistek <swistekxd@gmail.com>
2022-02-18 18:36:05 +0100

    make

--- client/src/PortfolioContainer/Home/Header/Header.js
+++ client/src/PortfolioContainer/Home/Header/Header.js
@@ -70,7 +70,7 @@ export default function Header() {
           <FontAwesomeIcon className='header-hamburger-bars' icon={faBars} />
         </div>
         <div className='header-logo'>
-          <span>...log("hello world")</span>
+          <span> hello world</span>
         </div>
         <div
           className={
